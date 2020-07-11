e1e9b2ed3c16887e907d6bc472011f5bab8d38dc
Hickacou <hickacou@gmail.com>
2020-07-11 17:10:17 +0200

    * Added eval command

---
+++ src/commands/OC Bot/eval.ts
@@ -0,0 +1,33 @@
+import { Message } from "discord.js";
+import Command from "../../core/base/Command";
+import OCBot from "../../core/base/Client";
+
+export = class extends Command {
+	constructor(client: OCBot) {
+		super(client, {
+			name: "eval",
+			desc: "Executes a Javascript expression (HickER only)",
+			module: "OC Bot",
+			usages: [
+				"<expression: String>"
+			],
+			aliases: ["js"],
+			whitelist: [client.owner]
+		});
+	}
+
+	public async setup(): Promise<void> {}
+
+	public async exe(message: Message, args: string[]): Promise<void> {
+		super.check(message, async () => {
+			const expression: string = args.join(" ");
+			try {
+				const res: any = await eval(expression);
+				if (res) message.channel.send(`\`${(res.toString) ? res.toString() : res}\``);
+				else message.channel.send("Expression executed with no error");
+			} catch (e) {
+				message.channel.send(`\`${e.toString()}\``);
+			}
+		});
+	}
+}
