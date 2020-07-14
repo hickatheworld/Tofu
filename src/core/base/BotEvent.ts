5968f8a4f9962b65c1274aa628c32fc8ba7cf33d
Hicka <hickacou@gmail.com>
2020-07-14 21:42:02 +0200

    Added props property on BotEvent

--- src/core/base/BotEvent.ts
+++ src/core/base/BotEvent.ts
@@ -4,10 +4,12 @@ export default abstract class BotEvent {
 	protected client: OCBot;
 	public name: string;
 	public once: boolean;
+	public props: Map<string, any>;
 	constructor(client: OCBot, name: string, once: boolean) {
 		this.client = client;
 		this.name = name;
 		this.once = once;
+		this.props = new Map<string, any>();
 	}
 
 	public abstract exe(...args: any): void;
