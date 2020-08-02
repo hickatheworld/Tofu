79fa3d4a5a004812b83d884d89cfa244e21f84c0
Hicka <hickacou@gmail.com>
2020-08-02 21:58:56 +0200

    * Added muted-role command

---
+++ src/commands/Moderation/muted-role.ts
@@ -0,0 +1,50 @@
+import Command from "../../core/base/Command";
+import OCBot from "../../core/base/Client";
+import { Message, Role } from "discord.js";
+import { parseRole } from "../../core/lib/Args";
+import GuildModerationSettings from "../../core/typedefs/GuildModerationSettings";
+
+export = class extends Command {
+	constructor(client: OCBot) {
+		super(client, {
+			name: "muted-role",
+			desc: "Sets the muted role on the server",
+			usages: [
+				"<muted-role: Role>",
+				"reset"
+			],
+			module: "Moderation",
+			perms: ["MANAGE_ROLES"]
+		});
+	}
+
+	public async setup(): Promise<void> { }
+
+	public async exe(message: Message, args: string[]): Promise<void> {
+		super.check(message, async () => {
+			if (!args[0]) {
+				const modSettings: GuildModerationSettings = await this.client.db.getModerationSettings(message.guild);
+				const role: Role = modSettings.mutedRole;
+				if (role) {
+					message.channel.send(`Muted role is set to **${role.name}** on this server.`);
+					return;
+				} else {
+					message.channel.send("No muted role is set on this server or the role has been deleted.");
+					return;
+				}
+			}
+			if (args[0].toLowerCase() === "reset") {
+				await this.client.db.setModerationSetting(message.guild, "mutedRole", null);
+				message.channel.send("✅ Reset muted role on this server.");
+				return;
+			}
+			if (!parseRole(args[0], message.guild)) {
+				message.channel.send("❌ Please specify a correct role.");
+				return;
+			}
+			const role: Role = parseRole(args[0], message.guild);
+			await this.client.db.setModerationSetting(message.guild, "mutedRole", role);
+			message.channel.send(`✅ Set muted role to **${role.name}**.`);
+		});
+	}
+}
