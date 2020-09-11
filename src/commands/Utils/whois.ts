787c9f5147b4b805cb7e7a65f78a4b9a54073f9c
Hicka <hickacou@gmail.com>
2020-09-11 12:26:50 +0200

    * Added oc!whois

---
+++ src/commands/Utils/whois.ts
@@ -0,0 +1,131 @@
+import { Message, User, EmbedField, MessageEmbed, GuildMember } from "discord.js";
+import Command from "../../core/base/Command";
+import OCBot from "../../core/base/Client";
+import { parseMember, parseUser } from "../../core/lib/Args";
+import { formatDate, formatDuration } from "../../core/lib/Time";
+
+
+export = class extends Command {
+	constructor(client: OCBot) {
+		super(client, {
+			name: "whois",
+			desc: "Get basic informations about a user",
+			module: "Utils",
+			usages: [
+				"[user: User]"
+			],
+		});
+	}
+
+	public async setup(): Promise<void> { }
+
+	public async exe(message: Message, args: string[]) {
+		super.check(message, () => {
+			var user: User;
+			var member: GuildMember;
+			if (parseUser(args[0], this.client)) {
+				user = parseUser(args[0], this.client);
+				member = parseMember(args[0], message.guild);
+				if (!member) {
+					message.channel.send("❌ User is not in this guild");
+					return;
+				}
+			}
+			else {
+				user = message.author;
+				member = message.member;
+			}
+			var embed: MessageEmbed;
+			try {
+				embed = new MessageEmbed()
+					.setTitle(`${user.tag} (${user.id})`)
+					.setColor(member.displayColor || "#AAAAAA")
+					.setThumbnail(user.displayAvatarURL({ dynamic: true }))
+					.addFields(
+						[
+							{
+								name: "Flags",
+								value: user.flags.toArray().map(i => "`" + i + "`").join(", ") || "No flags",
+								inline: true
+							},
+							{
+								name: "Mention",
+								value: user,
+								inline: true
+							},
+							{
+								name: "Avatar",
+								value: "[Link](" + user.displayAvatarURL({ dynamic: true }) + ")",
+								inline: true
+							},
+							{
+								name: "Account creation",
+								value: user.createdAt.toUTCString(),
+								inline: false
+							},
+							{
+								name: "Account age",
+								value: formatDuration(new Date(), user.createdAt, true),
+								inline: true
+							},
+							{
+								name: "Join age",
+								value: (member.joinedAt) ? formatDuration(new Date(), member.joinedAt, true) : "**Can't get join date**",
+								inline: false
+							},
+							{
+								name: "Join date",
+								value: (member.joinedAt) ? member.joinedAt : "**Can't get join date**",
+								inline: true
+							},
+							{
+								name: "Nickname",
+								value: (member.nickname) ? member.nickname : "None",
+								inline: true
+							}
+						]
+					);
+				if (member.premiumSince) {
+					embed.addFields([
+						{
+							name: "Boosting age",
+							value: formatDuration(new Date(), member.premiumSince),
+							inline: false,
+						},
+						{
+							name: "Boosting since",
+							value: member.premiumSince,
+							inline: true
+						}
+					]);
+				}
+				embed.addFields([
+					{
+						name: "Highest role",
+						value: member.roles.highest,
+						inline: false
+					},
+					{
+						name: "Color role",
+						value: (member.roles.color) ? member.roles.color : "None",
+						inline: true
+					},
+					{
+						name: "Hoisted role",
+						value: (member.roles.hoist) ? member.roles.hoist : "None",
+						inline: true
+					},
+					{
+						name: "Permisions",
+						value: member.permissions.toArray().map(i => "`" + i + "`").join(", "),
+						inline: false
+					}
+				])
+				message.channel.send("`" + user.id + "`", embed);
+			} catch (err) {
+				message.channel.send("Error");
+				console.log(embed);
+			}
+		});
+	}
+}
