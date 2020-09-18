4f471e84cbbfaa02a4a42be1345ff26888d030d6
Hicka <hickacou@gmail.com>
2020-09-18 13:35:00 +0200

    Added resume command

---
+++ src/commands/Music/resume.ts
@@ -0,0 +1,21 @@
+import { Message } from "discord.js";
+import OCBot from "../../core/base/Client";
+import Command from "../../core/base/Command";
+
+export = class extends Command {
+	constructor(client: OCBot) {
+		super(client, {
+			name: "resume",
+			desc: "Resumes audio",
+			module: "Music",
+		});
+	}
+
+	public async setup(): Promise<void> { }
+
+	public async exe(message: Message, args: string[]): Promise<void> {
+		this.check(message, async () => {
+			this.client.commands.get("play").exe(message, []);
+		});
+	}
+}
