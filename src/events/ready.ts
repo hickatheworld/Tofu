68a05e6dbbf1a8427794bd8635dbd35354b0ace0
Hicka <hickacou@gmail.com>
2020-10-12 21:38:42 +0200

    refactor: put default activity in .env
    This stops default activity from being dependant of codebase.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,5 @@
 import { yellow } from "chalk";
+import { ActivityType } from "discord.js";
 import BotEvent from "../core/base/BotEvent";
 import OCBot from "../core/base/Client";
 import * as log from "../core/lib/Log";
@@ -19,9 +20,13 @@ export = class extends BotEvent {
 		if (this.client.test) {
 			console.log(yellow("/!\\ Bot in test mode. All non-admin messages will be ignored /!\\"));
 		}
-		this.client.user.setActivity({
-			type: "PLAYING",
-			name: `intolerable stupidity`
-		});
+		const act: string = process.env.BOT_ACTIVITY;
+		if (!act) return;
+		const actType: ActivityType = ((["PLAYING", "STREAMING", "LISTENING", "WATCHING"].includes(process.env.BOT_ACTIVITY_TYPE)) ? process.env.BOT_ACTIVITY_TYPE : "PLAYING") as ActivityType;
+		if (actType === "STREAMING") {
+			this.client.user.setActivity(act, { type: actType, url: "https://twitch.tv/theuniqueocbot" });
+		} else {
+			this.client.user.setActivity(act, { type: actType });
+		}
 	}
 }
