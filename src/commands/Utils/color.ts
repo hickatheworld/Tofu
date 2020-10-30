b38cf5d7a30b7ef5d8efddd361b8320cb788c0d4
Hickacou <hickacou@gmail.com>
2020-10-30 03:09:13 +0100

    if this deploys I have a girlfriend tonight.

--- src/commands/Utils/color.ts
+++ src/commands/Utils/color.ts
@@ -77,7 +77,7 @@ export = class extends Command {
 		});
 	}
 }
-/* I am disgusted of myself, this command is terrible. */
+
 function randomColor(): string {
 	var color: string = "";
 	const chars: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
